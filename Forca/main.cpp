ee81721b6f231e71baf8c6b3effd49ccfca5c612
Felipe-Costa-Salino <felipecsalino2@gmail.com>
2025-09-25 14:34:31 -0300

    Update main.cpp
    Adicionando as bibliotecas, criando funções para o jogo da forca e colocando as palavras que vão ser sorteadas. Int main se a pessoa quiser jogar.

--- Forca/main.cpp
+++ Forca/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
 
+using namespace std;
 
+// Funções do jogo
+void inicializarJogo();
+void exibirForca(int erros);
+void exibirPalavraOculta(const char* palavraOculta, int tamanho);
+void exibirLetrasUsadas(const char* letrasUsadas);
+bool verificarLetra(char letra, const char* palavraSecreta, char* palavraOculta);
+bool jogarNovamente();
 
-int main(int argc, char** argv) {
-	return 0;
+// Variáveis globais
+char palavras[30][30] = {"BOB ESPONJA", "RICK AND MORTY", "HORA DE AVENTURA", "FUTURAMA", "O INCRIVEL MUNDO DE GUMBALL", "THE BOYS", "COBRA KAI", "OS SIMPSONS",
+						 "FAMILY GUY", "CLARENCIO O OTIMISTA", "APENAS UM SHOW", "BREAKING BAD", "SQUID GAME", "SCOOBY DOO", "LOKI", "INVENCIVEL", 
+						 "LOONEY TUNES", "PICA PAU", "PADRINHOS MAGICOS", "FLINSTONES", "PEPPA PIG", "SUPERNATURAL", "DEXTER", "BETTER CALL SAUL", "THE BIG BANG THEORY",
+						 "TEEN TITANS", "HENRY DANGER", "WANDINHA", "GAME OF THRONES", "STRANGER THINGS"};
+
+int main() {
+    srand(time(NULL));
+    char resposta[5];
+    
+    cout << "=== JOGO DA FORCA ===" << endl;
+    cout << "Tema: SERIES E DESENHOS ";
+    cout << "\n\nQuer jogar? (s/n)? ";
+    cin >> resposta;
+    
+    // Converter para minúsculas para comparação
+    for(int i = 0; resposta[i]; i++) {
+        resposta[i] = toupper(resposta[i]);
+    }
+    
+    if(strcmp(resposta, "S") == 0) {
+        cout << "Otimo! Vamos comecar!\n" << endl;
+        
+        do {
+            inicializarJogo();
+        } while(jogarNovamente());
+        
+        cout << "Obrigado por jogar! " << endl;
+    } else {
+        cout << "Ate a proxima!" << endl;
+    }
+    
+    return 0;
 }
